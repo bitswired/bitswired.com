ab40cf5e93f2a73faef359d47e086fc3ea4c0268
jimzer <jimi.vaubien@protonmail.com>
2022-02-02 14:09:58 +0000

    About feature

---
+++ src/features/about/index.ts
@@ -0,0 +1,3 @@
+export { default as Me } from './components/Me.mdx'
+export { default as Newsletter } from './components/Newsletter.mdx'
+export { default as Bitswired } from './components/Bitswired.mdx'
