bf8e72a0342bbdb3ea4218b280ea17f5a3e5982b
jimzer <jimi.vaubien@protonmail.com>
2022-02-02 14:12:19 +0000

    Next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+// eslint-disable-next-line
+const withPlugins = require('next-compose-plugins')
+
+// eslint-disable-next-line
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+const nextConfiguration = {
   reactStrictMode: true,
   swcMinify: true,
 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    withMDX({
+      pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    }),
+  ],
+  nextConfiguration
+)
