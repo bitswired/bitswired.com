8e2a29d3bb3bb458806267af12e7dbeebb16ef86
jimzer <jimi.vaubien@protonmail.com>
2022-02-02 14:01:02 +0000

    Project conifg

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export const CONFIG = {
+  navigation: {
+    paths: [
+      { name: 'home', path: '/' },
+      { name: 'blog', path: '/blog' },
+      { name: 'about', path: '/about' },
+    ],
+  },
+}
