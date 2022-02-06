e359a3d756b10470e584b2104dd5a35dc557715e
jimzer <jimi.vaubien@protonmail.com>
2022-02-06 15:29:58 +0000

    Fix import error mdx bundler

--- src/features/blog/utils/blog.ts
+++ src/features/blog/utils/blog.ts
@@ -1,10 +1,5 @@
 import JoiDate from '@joi/date'
 import * as JoiImport from 'joi'
-import { bundleMDX } from 'mdx-bundler'
-import { default as fsWithCallbacks } from 'fs'
-import path from 'path'
-import { CONFIG } from '@config'
-const fs = fsWithCallbacks.promises
 
 const Joi = JoiImport.extend(JoiDate)
 
@@ -17,14 +12,14 @@ const metaSchema = Joi.object({
   tags: Joi.array().items(Joi.string()).required(),
   series: Joi.string(),
   category: Joi.string().required(),
-  slug: Joi.string().required(),
+
   published: Joi.boolean().required(),
   datePublished: Joi.date().format('YYYY-MM-DD').raw().required(),
   dateModified: Joi.date().format('YYYY-MM-DD').raw().required(),
   images: Joi.array().items(Joi.string()).required(),
 })
 
-function validatePostMeta(obj: Record<string, unknown>, slug: string) {
+export function validatePostMeta(obj: Record<string, unknown>, slug: string) {
   const d = metaSchema.validate(obj, { convert: true })
   if (d.error) throw new Error(d.error)
   const postMeta: BlogPostMeta = d.value
@@ -32,30 +27,4 @@ function validatePostMeta(obj: Record<string, unknown>, slug: string) {
     throw new Error('Slug and file name not identical')
 
   return postMeta
-}
-
-export async function listAllSlugs() {
-  const posts = await fs.readdir(CONFIG.paths.content.blog)
-  return posts
-}
-
-export async function getPostBySlug(slug: string) {
-  const postPath = path.join(CONFIG.paths.content.blog, slug)
-
-  const { code, frontmatter } = await bundleMDX({
-    file: path.resolve(path.join(postPath, 'index.mdx')),
-    //cwd: path.resolve(postPath),
-    cwd: process.env.PWD,
-    esbuildOptions(options) {
-      // options.minify = false
-      options.target = ['esnext']
-      options.platform = 'node'
-
-      return options
-    },
-  })
-
-  const postMeta = validatePostMeta(frontmatter, slug)
-
-  return { code, postMeta }
 }
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,7 +1,11 @@
+import { bundleMDX } from 'mdx-bundler'
 import { getMDXComponent } from 'mdx-bundler/client'
 import React from 'react'
-import { BlogPostLayout } from '@features/blog'
-import { getPostBySlug, listAllSlugs } from '@features/blog'
+import { default as fsWithCallbacks } from 'fs'
+import path from 'path'
+import { CONFIG } from '@config'
+import { BlogPostLayout, validatePostMeta } from '@features/blog'
+const fs = fsWithCallbacks.promises
 
 interface BlogPostPageProps {
   postMeta: BlogPostMeta
@@ -23,6 +27,32 @@ export default function BlogPostPage({ postMeta, code }: BlogPostPageProps) {
       </BlogPostLayout>
     </>
   )
+}
+
+async function listAllSlugs() {
+  const posts = await fs.readdir(CONFIG.paths.content.blog)
+  return posts
+}
+
+async function getPostBySlug(slug: string) {
+  const postPath = path.join(CONFIG.paths.content.blog, slug)
+
+  const { code, frontmatter } = await bundleMDX({
+    file: path.resolve(path.join(postPath, 'index.mdx')),
+    //cwd: path.resolve(postPath),
+    cwd: process.env.PWD,
+    esbuildOptions(options) {
+      // options.minify = false
+      options.target = ['esnext']
+      options.platform = 'node'
+
+      return options
+    },
+  })
+
+  const postMeta = validatePostMeta(frontmatter, slug)
+
+  return { code, postMeta }
 }
 
 type Context = {
