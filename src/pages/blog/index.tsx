c7f7783f2c7c671b7f8bbf83ca8bb8c3b0029439
jimzer <jimi.vaubien@protonmail.com>
2022-02-10 16:24:02 +0000

    Blog index page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,49 @@
+import { bundleMDX } from 'mdx-bundler'
+import { default as fsWithCallbacks } from 'fs'
+import path from 'path'
+import { CONFIG } from '@config'
+import { BlogPostGrid, validatePostMeta } from '@features/blog'
+const fs = fsWithCallbacks.promises
+
+interface BlogPageProps {
+  postMetas: BlogPostMeta[]
+}
+
+export default function BlogPage({ postMetas }: BlogPageProps) {
+  return <BlogPostGrid postMetas={[...postMetas, ...postMetas, ...postMetas]} />
+}
+
+async function listAllSlugs() {
+  const posts = await fs.readdir(CONFIG.paths.content.blog)
+  return posts
+}
+
+async function getPostMeta(slug: string) {
+  const postPath = path.join(CONFIG.paths.content.blog, slug)
+
+  const { frontmatter } = await bundleMDX({
+    file: path.resolve(path.join(postPath, 'index.mdx')),
+    //cwd: path.resolve(postPath),
+    cwd: process.env.PWD,
+    esbuildOptions(options) {
+      // options.minify = false
+      options.target = ['esnext']
+      options.platform = 'node'
+      return options
+    },
+  })
+
+  const postMeta = validatePostMeta(frontmatter, slug)
+
+  return postMeta
+}
+
+export async function getStaticProps() {
+  const slugs = await listAllSlugs()
+
+  const postMetas = await Promise.all(slugs.map(getPostMeta))
+
+  return {
+    props: { postMetas },
+  }
+}
