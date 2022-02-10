4ec122f482d5a80aa0cb4ff586b43d22aae9c7ec
jimzer <jimi.vaubien@protonmail.com>
2022-02-10 16:24:23 +0000

    Add newsletter provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { MainLayout } from '@features/main-layout'
+import { NewsletterModalProvider } from '@features/newsletter'
 import { globalStyles } from '@lib/stitches'
 
 const queryClient = new QueryClient()
@@ -10,35 +11,37 @@ const queryClient = new QueryClient()
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles()
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Define default options
-          className: '',
-          duration: 5000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-          // Default options for specific types
-          success: {
-            duration: 3000,
-            theme: {
-              primary: 'green',
-              secondary: 'black',
+    <NewsletterModalProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: '',
+            duration: 5000,
+            style: {
+              background: '#363636',
+              color: '#fff',
             },
-          },
-        }}
-      />
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </QueryClientProvider>
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              theme: {
+                primary: 'green',
+                secondary: 'black',
+              },
+            },
+          }}
+        />
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </QueryClientProvider>
+    </NewsletterModalProvider>
   )
 }
 
